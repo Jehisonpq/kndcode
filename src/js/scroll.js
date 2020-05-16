5efdf3ebb425978ea1678f5eab73da8227bea97f
Jehison Perez <jehisonpq@gmail.com>
2020-05-16 15:25:37 -0500

    Ajuste jQuery

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,5 +1,4 @@
 import ScrollReveal from "scrollreveal";
-//var $ = require("jquery");
 
 const allScroll = () => {
   ScrollReveal().reveal(".section-left .subtitle", { delay: 100 });
@@ -44,17 +43,18 @@ const menuScroll = () => {
 const changeImage = (id1, id2) => {
   const secciones = document.getElementsByClassName("section-home");
 
-  $(window).scroll(function () {
-    let top_of_element = $(id1).offset().top;
+  jQuery(window).scroll(function () {
+    let top_of_element = jQuery(id1).offset().top;
     let top_element = top_of_element + 300;
-    let bottom_of_element = $(id1).offset().top + $(id1).outerHeight();
-    let bottom_of_screen = $(window).scrollTop() + $(window).height();
-    let top_of_screen = $(window).scrollTop();
+    let bottom_of_element =
+      jQuery(id1).offset().top + jQuery(id1).outerHeight();
+    let bottom_of_screen = jQuery(window).scrollTop() + jQuery(window).height();
+    let top_of_screen = jQuery(window).scrollTop();
 
     if (bottom_of_screen > top_element && top_of_screen < bottom_of_element) {
       if (id2) {
-        $(id1).find(".container-title").css({ top: "25%" });
-        $(id2).addClass("visible");
+        jQuery(id1).find(".container-title").css({ top: "25%" });
+        jQuery(id2).addClass("visible");
       }
 
       for (const seccion in secciones) {
@@ -66,14 +66,16 @@ const changeImage = (id1, id2) => {
       }
     } else {
       if (id2) {
-        $(id1).find(".container-title").css({ top: "70%" });
-        $(id2).removeClass("visible");
+        jQuery(id1).find(".container-title").css({ top: "70%" });
+        jQuery(id2).removeClass("visible");
       }
     }
 
     if (
-      $(window).scrollTop() >=
-      $(".container-app").innerHeight() - $("#section3").outerHeight() - 400
+      jQuery(window).scrollTop() >=
+      jQuery(".container-app").innerHeight() -
+        jQuery("#section3").outerHeight() -
+        400
     ) {
       document.getElementById("scroll").style.display = "none";
     } else {
