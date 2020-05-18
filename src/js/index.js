5810ca12e990999015b9b88138e3ead27d0ce55f
Jehison Perez <jehisonpq@gmail.com>
2020-05-17 19:22:05 -0500

    ajuste window onload js

--- src/js/index.js
+++ src/js/index.js
@@ -7,22 +7,22 @@ import { allSliders, sliderMobile } from "./slider";
 
 window.onload = function () {
   init();
+
+  menuHamburger();
+  menuMobileHidde();
+  allScroll();
+  menuScroll();
+
+  changeImage("#section-about", "#section-images-about");
+  changeImage("#section-techno", "#section-images-techno");
+  changeImage("#section-services", "#section-images-services");
+  changeImage("#section-cases", "#section-images-cases");
+  changeImage("#section1");
+
+  sliderMobile();
+  allSliders();
+
+  if (screen.width > 992) {
+    jQuery('[data-toggle="tooltip"]').tooltip();
+  }
 };
-
-menuHamburger();
-menuMobileHidde();
-allScroll();
-menuScroll();
-
-changeImage("#section-about", "#section-images-about");
-changeImage("#section-techno", "#section-images-techno");
-changeImage("#section-services", "#section-images-services");
-changeImage("#section-cases", "#section-images-cases");
-changeImage("#section1");
-
-sliderMobile();
-allSliders();
-
-if (screen.width > 992) {
-  jQuery('[data-toggle="tooltip"]').tooltip();
-}
